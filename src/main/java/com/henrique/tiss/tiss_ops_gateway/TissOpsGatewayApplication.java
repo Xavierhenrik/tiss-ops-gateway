8ba933c0db1d70c551fef173ea54e879702cef2d
SISXavier <hgsxavier@furb.br>
2025-09-20 14:57:54 -0300

    Feat: Projeto inicial gerado no Spring Initialazr

---
+++ src/main/java/com/henrique/tiss/tiss_ops_gateway/TissOpsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.tiss.tiss_ops_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TissOpsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TissOpsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrique/tiss/tiss_ops_gateway/TissOpsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.tiss.tiss_ops_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TissOpsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
